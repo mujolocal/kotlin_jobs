8a5e7dfbb657e80704659ee93b8e151a1a7408bf
muammar johnson <muammarj@gmail.com>
2021-02-12 14:14:07 -0600

    intent created goes to screen needs to be setup to go to screen after timer or some delay

--- app/src/main/java/com/example/weekend_hw_2/SplashActivity.kt
+++ app/src/main/java/com/example/weekend_hw_2/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weekend_hw_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.renderscript.ScriptGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,9 @@ class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
 
 
 
