33f345bdea162cf9e146c73fe3a6afe8451245a2
muammar johnson <muammarj@gmail.com>
2021-02-12 13:58:20 -0600

    splash activity set to initial activity

--- app/src/main/java/com/example/weekend_hw_2/SplashActivity.kt
+++ app/src/main/java/com/example/weekend_hw_2/SplashActivity.kt
@@ -1,7 +1,18 @@
 package com.example.weekend_hw_2
 
+import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.weekend_hw_2.databinding.ActivitySplashBinding
 
 class SplashActivity: AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+
+
+    }
 }
